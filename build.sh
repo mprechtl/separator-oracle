d9e2637bde16d672c17f082073fdedbf315dc6d8
Mike Prechtl <mike.prechtl@ecsec.de>
2018-05-25 23:57:18 +0200

    Add build script

---
+++ build.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+export LC_ALL=C.UTF-8
+export LANG=C.UTF-8
+
+# determine script path
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+cd ${DIR}
+virtualenv separator_oracle/venv
+
+source separator_oracle/venv/bin/activate
+
+# install modules and add active session to database
+pipenv install
+./manage.py runscript add_active_session --script-args _Username test _Password foobar _Valid 2099-04-04
+
+deactivate
+
+echo "Installation of Separator Oracle done..."
